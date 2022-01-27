cea79820c1722c9e2791e72cd10ebcbaba04e3ce
Niklas Burg <niklasburg1999.nb@gmail.com>
2022-01-27 16:34:05 +0100

    Added Images, started working on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'niklasburg.de';
+export class AppComponent implements OnInit {
+  images = ["code.jpg", "design.jpg", "keyboard.jpg"];
+  currentImage = 0; 
+  showImage = true;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+
+
+  updateImage() {
+    setInterval(() =>{
+      this.currentImage = (this.currentImage + 1) % this.images.length;
+    this.showImage = false; 
+
+    setTimeout(() => {
+      this.showImage = true;
+    }, 1);
+    }, 5000)
+  }
+
+
+
+
 }
